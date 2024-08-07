ef0c77d12245e8762f59dd2b566cffdbc7a56912
meskatabusayed <meskatabusayed@gmail.com>
2024-08-07 16:39:57 +0530

    conditionaly route faching

--- src/app/categories/[...features]/page.jsx
+++ src/app/categories/[...features]/page.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const page = ({params}) => {
-    console.log(params);
+    if(params.features.length === 3){
+        return( <div className='text-center'>
+            {params.features[2]}
+        </div>)
+    }
+    if(params.features.length === 2){
+        return( <div className='text-center'>
+            {params.features[1]}
+        </div>)
+    }
   return (
     <div>
       this is features in categories ...........
