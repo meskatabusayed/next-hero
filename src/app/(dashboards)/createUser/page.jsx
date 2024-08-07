71e2b85c5c6a1b537423265b91a41fd1da08156c
meskatabusayed <meskatabusayed@gmail.com>
2024-08-07 15:53:51 +0530

    route group done

---
+++ src/app/(dashboards)/createUser/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is dashboard create user route route
+    </div>
+  )
+}
+
+export default page;
---
+++ src/app/(dashboards)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is dashboard route
+    </div>
+  )
+}
+
+export default page
