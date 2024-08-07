0999518169bdb637a0bd0b983daa64f37529b1d6
meskatabusayed <meskatabusayed@gmail.com>
2024-08-07 16:31:00 +0530

    create multiple segment

---
+++ src/app/categories/[...features]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = ({params}) => {
+    console.log(params);
+  return (
+    <div>
+      this is features in categories ...........
+    </div>
+  )
+}
+
+export default page;
